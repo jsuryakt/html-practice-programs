5de1749e74b7d9d8781cc1e3710b900e939ec063
Jayasurya <jsuryakt@gmail.com>
2021-02-27 08:22:22 +0530

    Update to Valid Phone

--- valid_phone.php
+++ valid_phone.php
@@ -1,19 +1,12 @@
-<html>
-    <body>
-        <form method="POST" >
-            <input name="phone" >
-            <button>SUBMIT</button>
-        </form>
-        <?php
-        if(isset($_POST['phone'])) {
+<?php
+        if(!isset($PHONE['phone'])) {
             $phone = $_POST['phone'];
-            if(preg_match("/^0?[6-9][0-9]{9}$/", $phone)) {
-            echo "Phone $phone valid";
+            if (preg_match("/^0?[6-9][0-9]{9}$/", $phone)) {
+                echo "Phone $phone valid";
             }
             else {
                 echo "Please enter Valid Phone Number";
             }
         }
+        echo "<br><a href='JavaScript:history.go(-1);'>GO BACK</a>";
 ?>
-    </body>
-</html>
